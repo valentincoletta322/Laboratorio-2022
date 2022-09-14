bbcc0eb82d7498458d2ba4f1613198f6eeea0cf0
valentincoletta322 <81708912+valentincoletta322@users.noreply.github.com>
2022-09-14 10:53:05 -0300

    Update Elemento.java

--- ej1 interfaces/src/Elemento.java
+++ ej1 interfaces/src/Elemento.java
@@ -1,2 +1,15 @@
 abstract class Elemento {
+    private String nombre;
+
+    public Elemento() {
+        this.nombre = "nombre elemento";
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
