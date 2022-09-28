f9cd3df757a39ce1b57eabaaf4e9ee3dc538bc53
valentincoletta322 <81708912+valentincoletta322@users.noreply.github.com>
2022-09-27 21:49:35 -0300

    Delete interfaces2 directory

--- interfaces2/Alumno.java
+++
@@ -1,6 +0,0 @@
-public class Alumno {
-    private int dni;
-    private String apellido;
-    private String nombre;
-    private HashSet<Examen>
-}
--- interfaces2/src/MetodoDeAprobacion.java
+++
@@ -1,3 +0,0 @@
-public interface MetodoDeAprobacion {
-    boolean estaAprobado();
-}
